2bff4dcc0008b9fb8ca96d0a23029b7a407b7552
unknown <nicolas.rfontes@gmail.com>
2018-08-16 23:20:25 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,60 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Toast } from '@ionic-native/toast';
+import { DataServiceProvider } from '../../providers/data-service/data-service';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  products: any[] = [];
+  selectedProduct: any;
+  productFound: boolean = false;
 
+  constructor(public navCtrl: NavController,
+    private barcodeScanner: BarcodeScanner,
+    private toast: Toast,
+    public dataService: DataServiceProvider) {
+
+      this.dataService.getProducts()
+      .subscribe((response)=> {
+          this.products = response
+          console.log(this.products);
+      });
+
+  }
+
+  scan() {
+    this.selectedProduct = {};
+    this.barcodeScanner.scan().then((barcodeData) => {
+      if (barcodeData.format == "QR_CODE"){
+        location.href = barcodeData.text;
+      }else{
+        this.selectedProduct = this.products.find(product => product.plu === barcodeData.text);
+        if(this.selectedProduct !== undefined) {
+          this.productFound = true;
+          location.href = this.selectedProduct.price;
+        } else {
+          this.productFound = false;
+          this.toast.show(`Product not found`, '5000', 'center').subscribe(
+            toast => {
+              console.log(toast);
+            }
+          );
+        }
+      }
+      
+    }, (err) => {
+      this.toast.show(err, '5000', 'center').subscribe(
+        toast => {
+          console.log(toast);
+        }
+      );
+    });
   }
 
 }
